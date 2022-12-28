509643419eb149aadb9429d6a1d00c9106223ffc
Oleg Abdrashitov <olegabu73@gmail.com>
2022-12-28 18:41:29 +0400

    functions: event_logs contract_logs

--- foreign-server-redshift.sql
+++ foreign-server-redshift.sql
@@ -5,22 +5,18 @@ create extension if not exists postgres_fdw;
 create extension if not exists dblink;
 
 -- test-redshift-cluster-1.cujmiosxmo1p.eu-central-1.redshift.amazonaws.com
--- test-redshift-cluster-1.cujmiosxmo1p.eu-central-1.redshift.amazonaws.com
 
 drop server if exists redshift cascade;
 
-create server redshift
-  foreign data wrapper postgres_fdw
-options (host 'test-redshift-cluster-1.cujmiosxmo1p.eu-central-1.redshift.amazonaws.com', port '5439', dbname 'dev', sslmode 'require');
+create server redshift foreign data wrapper postgres_fdw options (host 'test-redshift-cluster-1.cujmiosxmo1p.eu-central-1.redshift.amazonaws.com', port '5439', dbname 'dev', sslmode 'require');
 
 drop user mapping if exists for postgres server redshift;
-create user mapping for postgres server redshift options ( user 'awsuser', password '');
+create user mapping for postgres server redshift options ( user 'awsuser', password 'Kekendrik789');
+
 
 drop server if exists redshift cascade;
 
-create server redshift
-  foreign data wrapper postgres_fdw
-options (host 'redshift-cluster-1.cujmiosxmo1p.eu-central-1.redshift.amazonaws.com', port '5439', dbname 'dev', sslmode 'require');
+create server redshift foreign data wrapper postgres_fdw options (host 'redshift-cluster-1.cujmiosxmo1p.eu-central-1.redshift.amazonaws.com', port '5439', dbname 'dev', sslmode 'require');
 
 drop user mapping if exists for postgres server redshift;
 create user mapping for postgres server redshift options ( user 'awsuser', password '');
