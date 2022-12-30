e5b18d6f87163b474bb05d0189cb6133212636df
Oleg Abdrashitov <olegabu73@gmail.com>
2022-12-30 13:17:22 +0400

    removing foreign server

--- schema-meta-redshift.sql
+++ schema-meta-redshift.sql
@@ -29,7 +29,6 @@ declare
   h varchar(66);
 begin
   select json, hash into u, h from eth.abi where signature = abi_signature;
-
   if u is null or h is null then raise exception 'cannot find abi for signature %', abi_signature; end if;
 
   w := 'where block_timestamp between ' || quote_literal(nvl(after_timestamp, 'now'::timestamp - '1 month'::interval)) || ' and ' || quote_literal(nvl(before_timestamp, 'now'::timestamp)) || ' and address = ' || quote_literal(contract_address) || ' and topics[0] = ' || quote_literal(h);
@@ -44,7 +43,9 @@ end
 $$
   language plpgsql;
 
--- begin; call event_logs('0x0aacfbec6a24756c20d41914f2caba817c0d8521', 'Transfer_address_indexed_from_address_indexed_to_uint256_amount', 'now'::timestamp - '1 month'::interval, 'now'::timestamp, 'desc', 10, 't'); select * from t; end;
+-- call event_logs('0x0aacfbec6a24756c20d41914f2caba817c0d8521', 'Transfer_address_from_address_to_uint256_amount_d', 't', 'now'::timestamp - '1 month'::interval, 'now'::timestamp, 'desc', 10);
+-- select * from t;
+
 
 drop procedure contract_logs(contract_address text, temp_table_name in varchar(128), "after_timestamp" timestamp, "before_timestamp" timestamp, order_dir text, "limit" int);
 
@@ -55,12 +56,10 @@ declare
   s varchar(2048);
   sunion varchar(2048);
 begin
-  w := 'where block_timestamp between ' || quote_literal(nvl(after_timestamp, 'now'::timestamp - '1 month'::interval)) || ' and ' || quote_literal(nvl(before_timestamp, 'now'::timestamp)) || ' and address = ' || quote_literal(contract_address);
-
   select into sunion listagg('select ' || quote_literal(name) || ' name,' || json || ' payload, transaction_hash, block_timestamp from eth.logs ' || w || ' and topics[0] = ' || quote_literal(hash), ' union all ') from eth.abi left join eth.event on abi.signature = event.abi_signature where event.contract_address = contract_address;
   if not found then raise exception 'cannot find abi for contract %',  contract_address; end if;
 
-  raise notice '%', sunion;
+  w := 'where block_timestamp between ' || quote_literal(nvl(after_timestamp, 'now'::timestamp - '1 month'::interval)) || ' and ' || quote_literal(nvl(before_timestamp, 'now'::timestamp)) || ' and address = ' || quote_literal(contract_address);
 
   s := 'select * from (' || sunion || ') order by block_timestamp ' || nvl(order_dir, 'desc') || ' limit ' || quote_literal(nvl("limit", 10));
   raise notice '%', s;
@@ -72,6 +71,5 @@ end
 $$
 language plpgsql;
 
-call contract_logs('0x0aacfbec6a24756c20d41914f2caba817c0d8521', 't', null, null, 'asc', 100);
-
-select * from t;
+-- call contract_logs('0x0aacfbec6a24756c20d41914f2caba817c0d8521', 't', null, null, 'asc', 100);
+-- select * from t;
