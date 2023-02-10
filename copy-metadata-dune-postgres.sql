08d4704f6e95f329bbab1764a6fb6039d94146a6
Oleg Abdrashitov <olegabu73@gmail.com>
2023-02-10 14:02:04 +0400

    added event_logs and contract_logs procedures for redshift, functions for postgres

--- copy-metadata-dune-postgres.sql
+++ copy-metadata-dune-postgres.sql
@@ -10,57 +10,57 @@ truncate table event cascade;
 \copy contract from './metadata/parse-dune-contracts-01-out-contract.csv' csv header;
 \copy event from './metadata/parse-dune-contracts-01-out-event.csv' csv header;
 
--- \copy app from './metadata/parse-dune-contracts-02-out-app.csv' csv header;
--- \copy abi from './metadata/parse-dune-contracts-02-out-abi.csv' csv header;
--- \copy contract from './metadata/parse-dune-contracts-02-out-contract.csv' csv header;
--- \copy event from './metadata/parse-dune-contracts-02-out-event.csv' csv header;
+\copy app from './metadata/parse-dune-contracts-02-out-app.csv' csv header;
+\copy abi from './metadata/parse-dune-contracts-02-out-abi.csv' csv header;
+\copy contract from './metadata/parse-dune-contracts-02-out-contract.csv' csv header;
+\copy event from './metadata/parse-dune-contracts-02-out-event.csv' csv header;
+
+\copy app from './metadata/parse-dune-contracts-03-out-app.csv' csv header;
+\copy abi from './metadata/parse-dune-contracts-03-out-abi.csv' csv header;
+\copy contract from './metadata/parse-dune-contracts-03-out-contract.csv' csv header;
+\copy event from './metadata/parse-dune-contracts-03-out-event.csv' csv header;
 --
--- \copy app from './metadata/parse-dune-contracts-03-out-app.csv' csv header;
--- \copy abi from './metadata/parse-dune-contracts-03-out-abi.csv' csv header;
--- \copy contract from './metadata/parse-dune-contracts-03-out-contract.csv' csv header;
--- \copy event from './metadata/parse-dune-contracts-03-out-event.csv' csv header;
+\copy app from './metadata/parse-dune-contracts-04-out-app.csv' csv header;
+\copy abi from './metadata/parse-dune-contracts-04-out-abi.csv' csv header;
+\copy contract from './metadata/parse-dune-contracts-04-out-contract.csv' csv header;
+\copy event from './metadata/parse-dune-contracts-04-out-event.csv' csv header;
 --
--- \copy app from './metadata/parse-dune-contracts-04-out-app.csv' csv header;
--- \copy abi from './metadata/parse-dune-contracts-04-out-abi.csv' csv header;
--- \copy contract from './metadata/parse-dune-contracts-04-out-contract.csv' csv header;
--- \copy event from './metadata/parse-dune-contracts-04-out-event.csv' csv header;
+\copy app from './metadata/parse-dune-contracts-05-out-app.csv' csv header;
+\copy abi from './metadata/parse-dune-contracts-05-out-abi.csv' csv header;
+\copy contract from './metadata/parse-dune-contracts-05-out-contract.csv' csv header;
+\copy event from './metadata/parse-dune-contracts-05-out-event.csv' csv header;
 --
--- \copy app from './metadata/parse-dune-contracts-05-out-app.csv' csv header;
--- \copy abi from './metadata/parse-dune-contracts-05-out-abi.csv' csv header;
--- \copy contract from './metadata/parse-dune-contracts-05-out-contract.csv' csv header;
--- \copy event from './metadata/parse-dune-contracts-05-out-event.csv' csv header;
+\copy app from './metadata/parse-dune-contracts-06-out-app.csv' csv header;
+\copy abi from './metadata/parse-dune-contracts-06-out-abi.csv' csv header;
+\copy contract from './metadata/parse-dune-contracts-06-out-contract.csv' csv header;
+\copy event from './metadata/parse-dune-contracts-06-out-event.csv' csv header;
 --
--- \copy app from './metadata/parse-dune-contracts-06-out-app.csv' csv header;
--- \copy abi from './metadata/parse-dune-contracts-06-out-abi.csv' csv header;
--- \copy contract from './metadata/parse-dune-contracts-06-out-contract.csv' csv header;
--- \copy event from './metadata/parse-dune-contracts-06-out-event.csv' csv header;
+\copy app from './metadata/parse-dune-contracts-07-out-app.csv' csv header;
+\copy abi from './metadata/parse-dune-contracts-07-out-abi.csv' csv header;
+\copy contract from './metadata/parse-dune-contracts-07-out-contract.csv' csv header;
+\copy event from './metadata/parse-dune-contracts-07-out-event.csv' csv header;
 --
--- \copy app from './metadata/parse-dune-contracts-07-out-app.csv' csv header;
--- \copy abi from './metadata/parse-dune-contracts-07-out-abi.csv' csv header;
--- \copy contract from './metadata/parse-dune-contracts-07-out-contract.csv' csv header;
--- \copy event from './metadata/parse-dune-contracts-07-out-event.csv' csv header;
+\copy app from './metadata/parse-dune-contracts-08-out-app.csv' csv header;
+\copy abi from './metadata/parse-dune-contracts-08-out-abi.csv' csv header;
+\copy contract from './metadata/parse-dune-contracts-08-out-contract.csv' csv header;
+\copy event from './metadata/parse-dune-contracts-08-out-event.csv' csv header;
 --
--- \copy app from './metadata/parse-dune-contracts-08-out-app.csv' csv header;
--- \copy abi from './metadata/parse-dune-contracts-08-out-abi.csv' csv header;
--- \copy contract from './metadata/parse-dune-contracts-08-out-contract.csv' csv header;
--- \copy event from './metadata/parse-dune-contracts-08-out-event.csv' csv header;
+\copy app from './metadata/parse-dune-contracts-09-out-app.csv' csv header;
+\copy abi from './metadata/parse-dune-contracts-09-out-abi.csv' csv header;
+\copy contract from './metadata/parse-dune-contracts-09-out-contract.csv' csv header;
+\copy event from './metadata/parse-dune-contracts-09-out-event.csv' csv header;
 --
--- \copy app from './metadata/parse-dune-contracts-09-out-app.csv' csv header;
--- \copy abi from './metadata/parse-dune-contracts-09-out-abi.csv' csv header;
--- \copy contract from './metadata/parse-dune-contracts-09-out-contract.csv' csv header;
--- \copy event from './metadata/parse-dune-contracts-09-out-event.csv' csv header;
+\copy app from './metadata/parse-dune-contracts-10-out-app.csv' csv header;
+\copy abi from './metadata/parse-dune-contracts-10-out-abi.csv' csv header;
+\copy contract from './metadata/parse-dune-contracts-10-out-contract.csv' csv header;
+\copy event from './metadata/parse-dune-contracts-10-out-event.csv' csv header;
 --
--- \copy app from './metadata/parse-dune-contracts-10-out-app.csv' csv header;
--- \copy abi from './metadata/parse-dune-contracts-10-out-abi.csv' csv header;
--- \copy contract from './metadata/parse-dune-contracts-10-out-contract.csv' csv header;
--- \copy event from './metadata/parse-dune-contracts-10-out-event.csv' csv header;
+\copy app from './metadata/parse-dune-contracts-11-out-app.csv' csv header;
+\copy abi from './metadata/parse-dune-contracts-11-out-abi.csv' csv header;
+\copy contract from './metadata/parse-dune-contracts-11-out-contract.csv' csv header;
+\copy event from './metadata/parse-dune-contracts-11-out-event.csv' csv header;
 --
--- \copy app from './metadata/parse-dune-contracts-11-out-app.csv' csv header;
--- \copy abi from './metadata/parse-dune-contracts-11-out-abi.csv' csv header;
--- \copy contract from './metadata/parse-dune-contracts-11-out-contract.csv' csv header;
--- \copy event from './metadata/parse-dune-contracts-11-out-event.csv' csv header;
---
--- \copy app from './metadata/parse-dune-contracts-12-out-app.csv' csv header;
--- \copy abi from './metadata/parse-dune-contracts-12-out-abi.csv' csv header;
--- \copy contract from './metadata/parse-dune-contracts-12-out-contract.csv' csv header;
--- \copy event from './metadata/parse-dune-contracts-12-out-event.csv' csv header;
+\copy app from './metadata/parse-dune-contracts-12-out-app.csv' csv header;
+\copy abi from './metadata/parse-dune-contracts-12-out-abi.csv' csv header;
+\copy contract from './metadata/parse-dune-contracts-12-out-contract.csv' csv header;
+\copy event from './metadata/parse-dune-contracts-12-out-event.csv' csv header;
--- copy-metadata-dune-redshift.sql
+++ copy-metadata-dune-redshift.sql
@@ -10,59 +10,59 @@ copy abi from 's3://summary.dev/metadata/parse-dune-contracts-01-out-abi.csv' ia
 copy contract from 's3://summary.dev/metadata/parse-dune-contracts-01-out-contract.csv' iam_role 'arn:aws:iam::729713441316:role/RedshiftRole' format csv ignoreheader 1;
 copy event from 's3://summary.dev/metadata/parse-dune-contracts-01-out-event.csv' iam_role 'arn:aws:iam::729713441316:role/RedshiftRole' format csv ignoreheader 1;
 
--- copy app from 's3://summary.dev/metadata/parse-dune-contracts-02-out-app.csv' iam_role 'arn:aws:iam::729713441316:role/RedshiftRole' format csv ignoreheader 1;
--- copy abi from 's3://summary.dev/metadata/parse-dune-contracts-02-out-abi.csv' iam_role 'arn:aws:iam::729713441316:role/RedshiftRole' format csv ignoreheader 1;
--- copy contract from 's3://summary.dev/metadata/parse-dune-contracts-02-out-contract.csv' iam_role 'arn:aws:iam::729713441316:role/RedshiftRole' format csv ignoreheader 1;
--- copy event from 's3://summary.dev/metadata/parse-dune-contracts-02-out-event.csv' iam_role 'arn:aws:iam::729713441316:role/RedshiftRole' format csv ignoreheader 1;
+copy app from 's3://summary.dev/metadata/parse-dune-contracts-02-out-app.csv' iam_role 'arn:aws:iam::729713441316:role/RedshiftRole' format csv ignoreheader 1;
+copy abi from 's3://summary.dev/metadata/parse-dune-contracts-02-out-abi.csv' iam_role 'arn:aws:iam::729713441316:role/RedshiftRole' format csv ignoreheader 1;
+copy contract from 's3://summary.dev/metadata/parse-dune-contracts-02-out-contract.csv' iam_role 'arn:aws:iam::729713441316:role/RedshiftRole' format csv ignoreheader 1;
+copy event from 's3://summary.dev/metadata/parse-dune-contracts-02-out-event.csv' iam_role 'arn:aws:iam::729713441316:role/RedshiftRole' format csv ignoreheader 1;
 --
--- copy app from 's3://summary.dev/metadata/parse-dune-contracts-03-out-app.csv' iam_role 'arn:aws:iam::729713441316:role/RedshiftRole' format csv ignoreheader 1;
--- copy abi from 's3://summary.dev/metadata/parse-dune-contracts-03-out-abi.csv' iam_role 'arn:aws:iam::729713441316:role/RedshiftRole' format csv ignoreheader 1;
--- copy contract from 's3://summary.dev/metadata/parse-dune-contracts-03-out-contract.csv' iam_role 'arn:aws:iam::729713441316:role/RedshiftRole' format csv ignoreheader 1;
--- copy event from 's3://summary.dev/metadata/parse-dune-contracts-03-out-event.csv' iam_role 'arn:aws:iam::729713441316:role/RedshiftRole' format csv ignoreheader 1;
+copy app from 's3://summary.dev/metadata/parse-dune-contracts-03-out-app.csv' iam_role 'arn:aws:iam::729713441316:role/RedshiftRole' format csv ignoreheader 1;
+copy abi from 's3://summary.dev/metadata/parse-dune-contracts-03-out-abi.csv' iam_role 'arn:aws:iam::729713441316:role/RedshiftRole' format csv ignoreheader 1;
+copy contract from 's3://summary.dev/metadata/parse-dune-contracts-03-out-contract.csv' iam_role 'arn:aws:iam::729713441316:role/RedshiftRole' format csv ignoreheader 1;
+copy event from 's3://summary.dev/metadata/parse-dune-contracts-03-out-event.csv' iam_role 'arn:aws:iam::729713441316:role/RedshiftRole' format csv ignoreheader 1;
 --
--- copy app from 's3://summary.dev/metadata/parse-dune-contracts-04-out-app.csv' iam_role 'arn:aws:iam::729713441316:role/RedshiftRole' format csv ignoreheader 1;
--- copy abi from 's3://summary.dev/metadata/parse-dune-contracts-04-out-abi.csv' iam_role 'arn:aws:iam::729713441316:role/RedshiftRole' format csv ignoreheader 1;
--- copy contract from 's3://summary.dev/metadata/parse-dune-contracts-04-out-contract.csv' iam_role 'arn:aws:iam::729713441316:role/RedshiftRole' format csv ignoreheader 1;
--- copy event from 's3://summary.dev/metadata/parse-dune-contracts-04-out-event.csv' iam_role 'arn:aws:iam::729713441316:role/RedshiftRole' format csv ignoreheader 1;
+copy app from 's3://summary.dev/metadata/parse-dune-contracts-04-out-app.csv' iam_role 'arn:aws:iam::729713441316:role/RedshiftRole' format csv ignoreheader 1;
+copy abi from 's3://summary.dev/metadata/parse-dune-contracts-04-out-abi.csv' iam_role 'arn:aws:iam::729713441316:role/RedshiftRole' format csv ignoreheader 1;
+copy contract from 's3://summary.dev/metadata/parse-dune-contracts-04-out-contract.csv' iam_role 'arn:aws:iam::729713441316:role/RedshiftRole' format csv ignoreheader 1;
+copy event from 's3://summary.dev/metadata/parse-dune-contracts-04-out-event.csv' iam_role 'arn:aws:iam::729713441316:role/RedshiftRole' format csv ignoreheader 1;
 --
--- copy app from 's3://summary.dev/metadata/parse-dune-contracts-05-out-app.csv' iam_role 'arn:aws:iam::729713441316:role/RedshiftRole' format csv ignoreheader 1;
--- copy abi from 's3://summary.dev/metadata/parse-dune-contracts-05-out-abi.csv' iam_role 'arn:aws:iam::729713441316:role/RedshiftRole' format csv ignoreheader 1;
--- copy contract from 's3://summary.dev/metadata/parse-dune-contracts-05-out-contract.csv' iam_role 'arn:aws:iam::729713441316:role/RedshiftRole' format csv ignoreheader 1;
--- copy event from 's3://summary.dev/metadata/parse-dune-contracts-05-out-event.csv' iam_role 'arn:aws:iam::729713441316:role/RedshiftRole' format csv ignoreheader 1;
+copy app from 's3://summary.dev/metadata/parse-dune-contracts-05-out-app.csv' iam_role 'arn:aws:iam::729713441316:role/RedshiftRole' format csv ignoreheader 1;
+copy abi from 's3://summary.dev/metadata/parse-dune-contracts-05-out-abi.csv' iam_role 'arn:aws:iam::729713441316:role/RedshiftRole' format csv ignoreheader 1;
+copy contract from 's3://summary.dev/metadata/parse-dune-contracts-05-out-contract.csv' iam_role 'arn:aws:iam::729713441316:role/RedshiftRole' format csv ignoreheader 1;
+copy event from 's3://summary.dev/metadata/parse-dune-contracts-05-out-event.csv' iam_role 'arn:aws:iam::729713441316:role/RedshiftRole' format csv ignoreheader 1;
 --
--- copy app from 's3://summary.dev/metadata/parse-dune-contracts-06-out-app.csv' iam_role 'arn:aws:iam::729713441316:role/RedshiftRole' format csv ignoreheader 1;
--- copy abi from 's3://summary.dev/metadata/parse-dune-contracts-06-out-abi.csv' iam_role 'arn:aws:iam::729713441316:role/RedshiftRole' format csv ignoreheader 1;
--- copy contract from 's3://summary.dev/metadata/parse-dune-contracts-06-out-contract.csv' iam_role 'arn:aws:iam::729713441316:role/RedshiftRole' format csv ignoreheader 1;
--- copy event from 's3://summary.dev/metadata/parse-dune-contracts-06-out-event.csv' iam_role 'arn:aws:iam::729713441316:role/RedshiftRole' format csv ignoreheader 1;
+copy app from 's3://summary.dev/metadata/parse-dune-contracts-06-out-app.csv' iam_role 'arn:aws:iam::729713441316:role/RedshiftRole' format csv ignoreheader 1;
+copy abi from 's3://summary.dev/metadata/parse-dune-contracts-06-out-abi.csv' iam_role 'arn:aws:iam::729713441316:role/RedshiftRole' format csv ignoreheader 1;
+copy contract from 's3://summary.dev/metadata/parse-dune-contracts-06-out-contract.csv' iam_role 'arn:aws:iam::729713441316:role/RedshiftRole' format csv ignoreheader 1;
+copy event from 's3://summary.dev/metadata/parse-dune-contracts-06-out-event.csv' iam_role 'arn:aws:iam::729713441316:role/RedshiftRole' format csv ignoreheader 1;
 --
--- copy app from 's3://summary.dev/metadata/parse-dune-contracts-07-out-app.csv' iam_role 'arn:aws:iam::729713441316:role/RedshiftRole' format csv ignoreheader 1;
--- copy abi from 's3://summary.dev/metadata/parse-dune-contracts-07-out-abi.csv' iam_role 'arn:aws:iam::729713441316:role/RedshiftRole' format csv ignoreheader 1;
--- copy contract from 's3://summary.dev/metadata/parse-dune-contracts-07-out-contract.csv' iam_role 'arn:aws:iam::729713441316:role/RedshiftRole' format csv ignoreheader 1;
--- copy event from 's3://summary.dev/metadata/parse-dune-contracts-07-out-event.csv' iam_role 'arn:aws:iam::729713441316:role/RedshiftRole' format csv ignoreheader 1;
+copy app from 's3://summary.dev/metadata/parse-dune-contracts-07-out-app.csv' iam_role 'arn:aws:iam::729713441316:role/RedshiftRole' format csv ignoreheader 1;
+copy abi from 's3://summary.dev/metadata/parse-dune-contracts-07-out-abi.csv' iam_role 'arn:aws:iam::729713441316:role/RedshiftRole' format csv ignoreheader 1;
+copy contract from 's3://summary.dev/metadata/parse-dune-contracts-07-out-contract.csv' iam_role 'arn:aws:iam::729713441316:role/RedshiftRole' format csv ignoreheader 1;
+copy event from 's3://summary.dev/metadata/parse-dune-contracts-07-out-event.csv' iam_role 'arn:aws:iam::729713441316:role/RedshiftRole' format csv ignoreheader 1;
 --
--- copy app from 's3://summary.dev/metadata/parse-dune-contracts-08-out-app.csv' iam_role 'arn:aws:iam::729713441316:role/RedshiftRole' format csv ignoreheader 1;
--- copy abi from 's3://summary.dev/metadata/parse-dune-contracts-08-out-abi.csv' iam_role 'arn:aws:iam::729713441316:role/RedshiftRole' format csv ignoreheader 1;
--- copy contract from 's3://summary.dev/metadata/parse-dune-contracts-08-out-contract.csv' iam_role 'arn:aws:iam::729713441316:role/RedshiftRole' format csv ignoreheader 1;
--- copy event from 's3://summary.dev/metadata/parse-dune-contracts-08-out-event.csv' iam_role 'arn:aws:iam::729713441316:role/RedshiftRole' format csv ignoreheader 1;
+copy app from 's3://summary.dev/metadata/parse-dune-contracts-08-out-app.csv' iam_role 'arn:aws:iam::729713441316:role/RedshiftRole' format csv ignoreheader 1;
+copy abi from 's3://summary.dev/metadata/parse-dune-contracts-08-out-abi.csv' iam_role 'arn:aws:iam::729713441316:role/RedshiftRole' format csv ignoreheader 1;
+copy contract from 's3://summary.dev/metadata/parse-dune-contracts-08-out-contract.csv' iam_role 'arn:aws:iam::729713441316:role/RedshiftRole' format csv ignoreheader 1;
+copy event from 's3://summary.dev/metadata/parse-dune-contracts-08-out-event.csv' iam_role 'arn:aws:iam::729713441316:role/RedshiftRole' format csv ignoreheader 1;
 --
--- copy app from 's3://summary.dev/metadata/parse-dune-contracts-09-out-app.csv' iam_role 'arn:aws:iam::729713441316:role/RedshiftRole' format csv ignoreheader 1;
--- copy abi from 's3://summary.dev/metadata/parse-dune-contracts-09-out-abi.csv' iam_role 'arn:aws:iam::729713441316:role/RedshiftRole' format csv ignoreheader 1;
--- copy contract from 's3://summary.dev/metadata/parse-dune-contracts-09-out-contract.csv' iam_role 'arn:aws:iam::729713441316:role/RedshiftRole' format csv ignoreheader 1;
--- copy event from 's3://summary.dev/metadata/parse-dune-contracts-09-out-event.csv' iam_role 'arn:aws:iam::729713441316:role/RedshiftRole' format csv ignoreheader 1;
+copy app from 's3://summary.dev/metadata/parse-dune-contracts-09-out-app.csv' iam_role 'arn:aws:iam::729713441316:role/RedshiftRole' format csv ignoreheader 1;
+copy abi from 's3://summary.dev/metadata/parse-dune-contracts-09-out-abi.csv' iam_role 'arn:aws:iam::729713441316:role/RedshiftRole' format csv ignoreheader 1;
+copy contract from 's3://summary.dev/metadata/parse-dune-contracts-09-out-contract.csv' iam_role 'arn:aws:iam::729713441316:role/RedshiftRole' format csv ignoreheader 1;
+copy event from 's3://summary.dev/metadata/parse-dune-contracts-09-out-event.csv' iam_role 'arn:aws:iam::729713441316:role/RedshiftRole' format csv ignoreheader 1;
 --
--- copy app from 's3://summary.dev/metadata/parse-dune-contracts-10-out-app.csv' iam_role 'arn:aws:iam::729713441316:role/RedshiftRole' format csv ignoreheader 1;
--- copy abi from 's3://summary.dev/metadata/parse-dune-contracts-10-out-abi.csv' iam_role 'arn:aws:iam::729713441316:role/RedshiftRole' format csv ignoreheader 1;
--- copy contract from 's3://summary.dev/metadata/parse-dune-contracts-10-out-contract.csv' iam_role 'arn:aws:iam::729713441316:role/RedshiftRole' format csv ignoreheader 1;
--- copy event from 's3://summary.dev/metadata/parse-dune-contracts-10-out-event.csv' iam_role 'arn:aws:iam::729713441316:role/RedshiftRole' format csv ignoreheader 1;
+copy app from 's3://summary.dev/metadata/parse-dune-contracts-10-out-app.csv' iam_role 'arn:aws:iam::729713441316:role/RedshiftRole' format csv ignoreheader 1;
+copy abi from 's3://summary.dev/metadata/parse-dune-contracts-10-out-abi.csv' iam_role 'arn:aws:iam::729713441316:role/RedshiftRole' format csv ignoreheader 1;
+copy contract from 's3://summary.dev/metadata/parse-dune-contracts-10-out-contract.csv' iam_role 'arn:aws:iam::729713441316:role/RedshiftRole' format csv ignoreheader 1;
+copy event from 's3://summary.dev/metadata/parse-dune-contracts-10-out-event.csv' iam_role 'arn:aws:iam::729713441316:role/RedshiftRole' format csv ignoreheader 1;
 --
--- copy app from 's3://summary.dev/metadata/parse-dune-contracts-11-out-app.csv' iam_role 'arn:aws:iam::729713441316:role/RedshiftRole' format csv ignoreheader 1;
--- copy abi from 's3://summary.dev/metadata/parse-dune-contracts-11-out-abi.csv' iam_role 'arn:aws:iam::729713441316:role/RedshiftRole' format csv ignoreheader 1;
--- copy contract from 's3://summary.dev/metadata/parse-dune-contracts-11-out-contract.csv' iam_role 'arn:aws:iam::729713441316:role/RedshiftRole' format csv ignoreheader 1;
--- copy event from 's3://summary.dev/metadata/parse-dune-contracts-11-out-event.csv' iam_role 'arn:aws:iam::729713441316:role/RedshiftRole' format csv ignoreheader 1;
+copy app from 's3://summary.dev/metadata/parse-dune-contracts-11-out-app.csv' iam_role 'arn:aws:iam::729713441316:role/RedshiftRole' format csv ignoreheader 1;
+copy abi from 's3://summary.dev/metadata/parse-dune-contracts-11-out-abi.csv' iam_role 'arn:aws:iam::729713441316:role/RedshiftRole' format csv ignoreheader 1;
+copy contract from 's3://summary.dev/metadata/parse-dune-contracts-11-out-contract.csv' iam_role 'arn:aws:iam::729713441316:role/RedshiftRole' format csv ignoreheader 1;
+copy event from 's3://summary.dev/metadata/parse-dune-contracts-11-out-event.csv' iam_role 'arn:aws:iam::729713441316:role/RedshiftRole' format csv ignoreheader 1;
 --
--- copy app from 's3://summary.dev/metadata/parse-dune-contracts-12-out-app.csv' iam_role 'arn:aws:iam::729713441316:role/RedshiftRole' format csv ignoreheader 1;
--- copy abi from 's3://summary.dev/metadata/parse-dune-contracts-12-out-abi.csv' iam_role 'arn:aws:iam::729713441316:role/RedshiftRole' format csv ignoreheader 1;
--- copy contract from 's3://summary.dev/metadata/parse-dune-contracts-12-out-contract.csv' iam_role 'arn:aws:iam::729713441316:role/RedshiftRole' format csv ignoreheader 1;
--- copy event from 's3://summary.dev/metadata/parse-dune-contracts-12-out-event.csv' iam_role 'arn:aws:iam::729713441316:role/RedshiftRole' format csv ignoreheader 1;
+copy app from 's3://summary.dev/metadata/parse-dune-contracts-12-out-app.csv' iam_role 'arn:aws:iam::729713441316:role/RedshiftRole' format csv ignoreheader 1;
+copy abi from 's3://summary.dev/metadata/parse-dune-contracts-12-out-abi.csv' iam_role 'arn:aws:iam::729713441316:role/RedshiftRole' format csv ignoreheader 1;
+copy contract from 's3://summary.dev/metadata/parse-dune-contracts-12-out-contract.csv' iam_role 'arn:aws:iam::729713441316:role/RedshiftRole' format csv ignoreheader 1;
+copy event from 's3://summary.dev/metadata/parse-dune-contracts-12-out-event.csv' iam_role 'arn:aws:iam::729713441316:role/RedshiftRole' format csv ignoreheader 1;
 
 -- select * from abi limit 10;
