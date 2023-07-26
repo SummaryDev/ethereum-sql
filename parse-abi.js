07f50ed90894f02b7650e04644d35371fda6b3d3
Oleg Abdrashitov <olegabu73@gmail.com>
2023-07-26 13:54:32 -0400

    topics[] to topic0 topic1; added parseEventAbiWithLabels

--- parse-abi.js
+++ parse-abi.js
@@ -1,5 +1,5 @@
 import keccak256 from 'keccak256';
-import {supportedTypesDict, typeToName, eventTableName} from './util.js'
+import {supportedTypesDict, typeToName, eventTableName, contractEventTableName} from './util.js'
 
 export function parseEventAbi(abi) {
   if (abi.anonymous) {
@@ -65,7 +65,7 @@ export function parseEventAbi(abi) {
 
     if (input.indexed) {
       args.push(2)
-      args.push(`topics[${counterTopic}]::text`)
+      args.push(`topic${counterTopic}::text`)
       typesWithNames.push(`${input.type}_${input.name}`)
       typesWithNamesIndexed.push(`${input.type} indexed ${input.name}`)
       counterTopic++
@@ -101,4 +101,63 @@ export function parseEventAbi(abi) {
     json: json,
     // columns: columns.join()
   }
+}
+
+export function parseEventAbiWithLabels(d, labels, contracts, abis, events, contractEvents) {
+  if (!d.abi) {
+    console.warn(`skipping record for missing abi in ${JSON.stringify(d)}`)
+    return
+  }
+
+  if (!d.address) {
+    console.warn(`skipping record for missing address in ${JSON.stringify(d)}`)
+    return
+  }
+
+  if (!d.namespace) {
+    console.warn(`skipping record for missing namespace in ${JSON.stringify(d)}`)
+    return
+  }
+
+  if (!d.name) {
+    console.warn(`skipping record for missing name in ${JSON.stringify(d)}`)
+    return
+  }
+
+  if (d.address.length !== 42) {
+    console.warn(`skipping record ${d.address} for address length ${d.address.length}`)
+    return
+  }
+
+  labels.add(d.namespace)
+
+  const address = d.address.replace('\\', '0')
+
+  contracts.set(address, {
+    label: d.namespace,
+    name: d.name
+  })
+
+  const dabis = d.abi.filter(o => o.type === 'event' && !o.anonymous && o.inputs && o.inputs.length > 0)
+
+  for (const a of dabis) {
+    const abi = parseEventAbi(a)
+
+    if (abi) {
+      abis.set(abi.signature, abi)
+
+      events.add(JSON.stringify({
+        contract_address: address,
+        abi_signature: abi.signature,
+      }))
+
+      contractEvents.set(contractEventTableName(d.namespace, d.name, abi.name), {
+        contract_label: d.namespace,
+        contract_name: d.name,
+        abi_signature: abi.signature,
+        abi_table_name: abi.table_name,
+      })
+    }
+  }
+
 }
