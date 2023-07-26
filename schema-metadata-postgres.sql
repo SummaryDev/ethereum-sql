c8dd6d27e9ba10a3975a124e928ae2288137543e
Oleg Abdrashitov <olegabu73@gmail.com>
2023-07-26 14:01:27 -0400

    use schema metadata for abis, labels, contracts, events

--- schema-metadata-postgres.sql
+++ schema-metadata-postgres.sql
@@ -1,33 +1,27 @@
--- create schema if not exists eth;
--- set search_path to eth;
 
-create schema if not exists ethereum;
-set search_path to ethereum;
-
--- drop table if exists app cascade;
+create schema if not exists metadata;
+set search_path to metadata;
 
 drop table if exists label cascade;
 create table label (name text primary key);
 
 drop table if exists contract cascade;
-
 create table contract (address text primary key, name text, label text references label);
 create index on contract (label);
 create index on contract (name);
 
 drop table if exists abi cascade;
-
 create table abi (signature text primary key, name text not null, hash text not null, unpack text not null, json text not null, canonical text not null, table_name text not null);
 create index on abi (hash);
 create index on abi (name);
 create index on abi (canonical);
 
 drop table if exists event cascade;
-
 create table event (contract_address text references contract, abi_signature text references abi, primary key (contract_address, abi_signature));
 create index on event (contract_address);
 create index on event (abi_signature);
 
+-- needed for graphile
 -- comment on table label is E'@listSuffix omit';
 -- comment on table contract is E'@listSuffix omit';
 -- comment on table abi is E'@listSuffix omit';
@@ -35,7 +29,6 @@ create index on event (abi_signature);
 comment on constraint contract_label_fkey on contract is E'@fieldName app\n@foreignFieldName contracts';
 comment on constraint event_abi_signature_fkey on event is E'@fieldName abi\n@foreignFieldName events';
 comment on constraint event_contract_address_fkey on event is E'@fieldName contract\n@foreignFieldName events';
-
 -- comment on constraint contract_label_fkey on contract is null;
 -- comment on constraint event_abi_signature_fkey on event is null;
 -- comment on constraint event_contract_address_fkey on event is null;
